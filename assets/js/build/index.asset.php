53d3a3dfb5796210dc4f17a8501010520ef049b2
Homaye Tabesh <tabshhh5@gmail.com>
2026-01-06 16:57:01 +0000

    Revert "Add user dashboard with mobile OTP authentication via Meli Payamak"

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'f03be863069a905b8682');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '2a73d22b65e10f049ac4');
